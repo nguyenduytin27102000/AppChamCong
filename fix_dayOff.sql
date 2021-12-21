98b33414f236b2cfdf4e6d5106639d432a832bff
Phước Hữu <phuocsama009@gmail.com>
2021-12-21 18:02:08 +0700

    Add db

--- fix_dayOff.sql
+++
@@ -1,9 +0,0 @@
-use TimeKeepingDB
-drop table TimeOffShift
-ALTER TABLE DayOff
-ADD FromHour varchar(5) not null,
-ToHour varchar(5) not null;
-
-EXEC sp_rename 'dbo.DayOff.DayOff', 'DayOffAt', 'COLUMN';
-EXEC sp_rename 'dbo.TimeOffRequestState.TimeOffRequestState', 'TimeOffRequestStateName', 'COLUMN';
-EXEC sp_rename 'dbo.NumberOfShift.NumberOfShift', 'Count', 'COLUMN';
