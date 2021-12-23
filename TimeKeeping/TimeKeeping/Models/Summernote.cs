ee6c6e1358acb178dcf0d09d2df079f40844c4a9
kimthoa3012 <thoakim3012@gmail.com>
2021-12-23 15:21:08 +0700

    Feedback for ticket 20211207-011

---
+++ TimeKeeping/TimeKeeping/Models/Summernote.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeKeeping.Models
+{
+    public class Summernote
+    {
+        public Summernote(string iDEditor, bool load = true)
+        {
+            IDEditor = iDEditor;
+            LoadLibrary = load;
+        }
+        public string IDEditor { get; set; }
+        public bool LoadLibrary { get; set; }
+        public int height { get; set; } = 200;
+        public string toolbar { get; set; } = @"
+            [
+              ['style', ['style']],
+              ['font', ['bold', 'underline', 'clear']],
+              ['color', ['color']],
+              ['para', ['ul', 'ol', 'paragraph']],
+              ['table', ['table']],
+              ['insert', ['link', 'picture', 'video']],
+              ['view', ['fullscreen', 'codeview', 'help']]
+            ]
+        ";
+
+
+    }
+}
